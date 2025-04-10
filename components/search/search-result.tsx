643f3ca0cb0a12e071fe16f3719be95707fe4525
Ishan Tonk <ishantonk.w@gmail.com>
2025-04-11 03:58:17 +0530

    Refactor ShowCourses component; remove unnecessary href prop from CourseCard

--- components/search/search-result.tsx
+++ components/search/search-result.tsx
@@ -139,11 +139,7 @@ function ShowCourses({
             </h2>
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                 {data.map((course) => (
-                    <CourseCard
-                        key={course.id}
-                        course={course}
-                        href={`/courses/${course.slug}`}
-                    />
+                    <CourseCard key={course.id} course={course} />
                 ))}
             </div>
         </div>
