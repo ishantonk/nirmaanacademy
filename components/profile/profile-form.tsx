99541a22230281c103f09deb64c4ce3fda79b669
Ishan Tonk <ishantonk.w@gmail.com>
2025-04-13 19:50:39 +0530

    Update profile-form.tsx

--- components/profile/profile-form.tsx
+++ components/profile/profile-form.tsx
@@ -211,7 +211,7 @@ export function ProfileForm() {
                 <Form {...form}>
                     <form
                         onSubmit={form.handleSubmit(onSubmit)}
-                        className="space-y-4"
+                        className="space-y-6"
                     >
                         {/* Profile Image Field */}
                         <FormField
